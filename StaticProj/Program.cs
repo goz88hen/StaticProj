b028d9fdb31709531c2fc37658224bec7dae8df3
Henry (Hendra) Gozali <123764602+goz88hen@users.noreply.github.com>
2023-04-27 23:19:51 -0700

    Exercise 1 project is complete.

---
+++ StaticProj/Program.cs
@@ -0,0 +1,21 @@
+﻿using StaticProj;
+using System;
+
+namespace StaticProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var celsius = TempConverter.FahrenheitToCelsius(68);
+            var fahrenheit = TempConverter.CelsiusToFahrenheit(20);
+
+            Console.WriteLine($"{celsius} celsius");
+            Console.WriteLine($"{fahrenheit} fahrenheit");
+
+        }
+
+    }
+}
+
+
